2a2906097f004da13d31eb102582438458c17831
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-13 17:28:25 +0200

    obsolete

--- atmospheric_cherenkov_response/grid/__init__.py
+++ atmospheric_cherenkov_response/grid/__init__.py
@@ -1,20 +1,3 @@
-import numpy as np
 from . import bounding_box
 from . import viewcone
 from . import instrument
-
-
-def project_bounding_box_onto_ground(
-    bounding_box_width_x,
-    bounding_box_width_y,
-    bounding_box_height_z,
-    viewcone_half_angle_deg,
-    viewcone_azimuth_deg,
-    viewcone_zenith_distance_deg,
-):
-    bb = np.array([bounding_box_x, bounding_box_y, bounding_box_z,])
-
-    """
-    project all eight corners of the instrument's bounding box
-    on the ground (x-y-plane)
-    """
