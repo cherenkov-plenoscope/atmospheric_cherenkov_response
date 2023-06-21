307c6446c9eb953d1c9fab0eadf13aa282442bec
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-21 17:02:38 +0200

    blk and screen intersection

--- atmospheric_cherenkov_response/instrument/dummy.py
+++ atmospheric_cherenkov_response/instrument/dummy.py
@@ -138,29 +138,27 @@ def estimate_rate_in_trigger_pixel_from_night_sky_background(
 def estimate_trigger_response(
     dummy_instrument,
     night_sky_background_differential_flux_per_m2_per_sr_per_s_per_m,
-    cherenkov_bunches_Tins,
+    cherenkov_bunches,
     prng,
 ):
     # remove bunches outside of mirror
     # --------------------------------
     bunch_r2 = (
-        cherenkov_bunches_Tins[:, BUNCH.X] ** 2
-        + cherenkov_bunches_Tins[:, BUNCH.Y] ** 2
+        cherenkov_bunches[:, BUNCH.X] ** 2 + cherenkov_bunches[:, BUNCH.Y] ** 2
     )
     mirror_r2 = (0.5 * dummy_instrument["mirror_diameter_m"]) ** 2
     mask_on_mirror = r2 <= mirror_r2
 
-    cherenkov_bunches_Tins = cherenkov_bunches_Tins[mask_on_mirror]
+    cherenkov_bunches = cherenkov_bunches[mask_on_mirror]
 
     # remove bunches absorbed in atmosphere
     # -------------------------------------
-    prob = prng.uniform(low=0, high=1, size=len(cherenkov_bunches_Tins))
-    mask_passing_atmosphere = cherenkov_bunches_Tins[:, BUNCH.BSIZE] >= prob
-    cherenkov_bunches_Tins = cherenkov_bunches_Tins[prob]
+    prob = prng.uniform(low=0, high=1, size=len(cherenkov_bunches))
+    mask_passing_atmosphere = cherenkov_bunches[:, BUNCH.BSIZE] >= prob
+    cherenkov_bunches = cherenkov_bunches[prob]
 
     # assign photon to pixel
     # ----------------------
-
 
 
 def make_trigger_pixel_centers(
@@ -184,7 +182,58 @@ def make_trigger_pixel_centers(
     return pos_xyz[:, 0:2]
 
 
+def normalize_axis_1(v):
+    no = np.linalg.norm(v, axis=1)
+    v[:, 0] /= no
+    v[:, 1] /= no
+    v[:, 2] /= no
+    return v
 
-def assign_photons_to_pixel(pixel_centers, pixel_radius):
-    pass
-    #tree = scipy.spatial.cKDTree(data=)
+
+def compute_photon_x_y_intersections_on_screen(
+    focal_length, screen_distance, cx, cy, x, y
+):
+    """
+    Based on the thin-lens.
+
+    Parameters
+    ----------
+    focal_length : float
+        The focal-length of the imaging optics.
+    screen_distance : float
+        The distance where the screen is placed.
+        If screen_distance == focal_length, the screen is focused to infinity.
+    cx, cy, x, y : arrays of floats
+        The photons directions (cx, cy) and impacts (x, y) relative to
+        the aperture's principal plane.
+    """
+    assert focal_length > 0.0
+    assert screen_distance > 0.0
+    num = len(cx)
+    assert num == len(cy)
+    assert num == len(x)
+    assert num == len(y)
+
+    f = focal_length
+    d = screen_distance
+    tan = np.tan
+
+    intersections_on_focal_plane = np.array(
+        [-f * tan(cx), -f * tan(cy), f * np.ones(num)]
+    )
+
+    supports_on_aperture = np.array([x, y, np.zeros(num)])
+
+    directions_leaving_aperture = (
+        intersections_on_focal_plane - supports_on_aperture
+    )
+    directions_leaving_aperture = normalize_axis_1(directions_leaving_aperture)
+
+    scale_factor = screen_distance / directions_leaving_aperture[:, 2]
+
+    intersections_on_screen = (
+        supports_on_aperture
+        + (scale_factors * directions_leaving_aperture.T).T
+    )
+
+    return intersections_on_screen
