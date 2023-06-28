307aaa28071d37cde763ca1d98690ebc872681f4
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-28 13:34:54 +0200

    define pointing

--- atmospheric_cherenkov_response/pointing.py
+++ atmospheric_cherenkov_response/pointing.py
@@ -14,20 +14,13 @@ def make_pointing_key(pointing):
     )
 
 
-def init_civil_rotation_of_principal_aperture_plane(pointing, mount):
+def make_civil_rotation_of_principal_aperture_plane(pointing, mount):
     """
-    cable-robot-mount
-    -----------------
-    no rotation in z-axis
-
-    altitude-azimuth-mount
-    ----------------------
-    rotates in z-axis
     """
-    if mount == "cable-robot-mount":
-        return {"repr": None}
-    elif mount == "altitude-azimuth-mount":
-        return {"repr": None}
+    if mount == "cable_robot_mount":
+        return make_civil_rotation_for_cable_robot_mount(**pointing)
+    elif mount == "altitude_azimuth_mount":
+        return make_civil_rotation_for_altitude_azimuth_mount(**pointing)
     else:
         raise KeyError("No such mount: '{:s}'".format(mount))
 
@@ -42,3 +35,39 @@ def make_civil_rotation_for_altitude_azimuth_mount(azimuth_deg, zenith_deg):
         "repr": "tait_bryan",
         "xyz_deg": np.array([0.0, -zenith_deg, -azimuth_deg]),
     }
+
+
+def make_civil_rotation_for_cable_robot_mount(
+    azimuth_deg, zenith_deg, eps_deg=1e-5, eps_1=1e-5
+):
+    """
+    does not rotate in z-axis
+    """
+    if np.abs(zenith_deg) > eps_deg:
+        rot_civil_altaz = make_civil_rotation_for_altitude_azimuth_mount(
+            azimuth_deg=azimuth_deg, zenith_deg=zenith_deg,
+        )
+        t_civil_altaz = {"rot": rot_civil_altaz, "pos": [0, 0, 0]}
+        t_altaz = homogeneous_transformation.compile(t_civil_altaz)
+        z = np.array([0, 0, 1])
+        z_t = homogeneous_transformation.transform_orientation(t_altaz, z)
+        assert np.abs(np.linalg.norm(z_t) - 1) < eps_1
+        pointing_direction = z_t
+        rot_axis = np.cross(z.T, pointing_direction)
+        rot_axis /= np.linalg.norm(rot_axis)
+        assert np.abs(np.linalg.norm(rot_axis) - 1) < eps_1
+        angle_z_and_pointing_rad = np.arccos(np.dot(pointing_direction, z))
+        angle_z_and_pointing_deg = np.rad2deg(angle_z_and_pointing_rad)
+        assert np.abs(zenith_deg) - angle_z_and_pointing_deg < eps_1
+        rot = {
+            "repr": "axis_angle",
+            "axis": rot_axis,
+            "angle_deg": angle_z_and_pointing_deg,
+        }
+    else:
+        rot = {
+            "repr": "tait_bryan",
+            "xyz_deg": [0, 0, 0],
+        }
+
+    return rot
--- atmospheric_cherenkov_response/tests/test_pointing.py
+++ atmospheric_cherenkov_response/tests/test_pointing.py
@@ -2,37 +2,96 @@ import atmospheric_cherenkov_response as acr
 import homogeneous_transformation
 import numpy as np
 
+ISQ = 1 / np.sqrt(2)
+
+CASES_AZIMUTH_ZERO = [
+    {"az": 0, "zd": 0, "x": [1, 0, 0], "y": [0, 1, 0], "z": [0, 0, 1]},
+    {"az": 0, "zd": 90, "x": [0, 0, -1], "y": [0, 1, 0], "z": [1, 0, 0]},
+    {
+        "az": 0,
+        "zd": 45,
+        "x": [ISQ, 0, -ISQ],
+        "y": [0, 1, 0],
+        "z": [ISQ, 0, ISQ],
+    },
+    {
+        "az": 0,
+        "zd": -45,
+        "x": [ISQ, 0, ISQ],
+        "y": [0, 1, 0],
+        "z": [-ISQ, 0, ISQ],
+    },
+]
+
 
 def assert_close(a, b, eps=1e-6):
     assert np.linalg.norm(a - b) <= eps
 
 
-def transform_xyz_altitude_azimuth_mount(pointing):
-    rot_civil = acr.pointing.make_civil_rotation_for_altitude_azimuth_mount(
-        **pointing
-    )
-
+def transform_xyz(rot_civil):
     origin = np.array([0, 0, 0])
-
     t_civil = {"rot": rot_civil, "pos": origin}
-
     t = homogeneous_transformation.compile(t_civil)
-
     x = np.array([1, 0, 0])
     y = np.array([0, 1, 0])
     z = np.array([0, 0, 1])
-
     x_t = homogeneous_transformation.transform_orientation(t, x)
     y_t = homogeneous_transformation.transform_orientation(t, y)
     z_t = homogeneous_transformation.transform_orientation(t, z)
-
     return x_t, y_t, z_t
 
 
-def test_altitude_azimuth_mount():
-    x, y, z = transform_xyz_altitude_azimuth_mount(
-        acr.pointing.init(azimuth_deg=0.0, zenith_deg=0.0)
+def assert_case(mount, case):
+    c = case
+    rot_civil = acr.pointing.make_civil_rotation_of_principal_aperture_plane(
+        pointing=acr.pointing.init(azimuth_deg=c["az"], zenith_deg=c["zd"]),
+        mount=mount,
     )
-    assert_close(x, [1, 0, 0])
-    assert_close(x, [1, 0, 0])
-    assert_close(x, [1, 0, 0])
+    x, y, z = transform_xyz(rot_civil)
+    assert_close(x, c["x"])
+    assert_close(y, c["y"])
+    assert_close(z, c["z"])
+
+
+def test_altitude_azimuth_mount():
+    for case in CASES_AZIMUTH_ZERO:
+        assert_case(mount="altitude_azimuth_mount", case=case)
+
+    cases = [
+        {"az": 90, "zd": 0, "x": [0, 1, 0], "y": [-1, 0, 0], "z": [0, 0, 1]},
+        {"az": 180, "zd": 0, "x": [-1, 0, 0], "y": [0, -1, 0], "z": [0, 0, 1]},
+        {"az": 270, "zd": 0, "x": [0, -1, 0], "y": [1, 0, 0], "z": [0, 0, 1]},
+    ]
+
+    for case in cases:
+        assert_case(mount="altitude_azimuth_mount", case=case)
+
+
+def test_cable_robot_mount():
+    for case in CASES_AZIMUTH_ZERO:
+        assert_case(mount="cable_robot_mount", case=case)
+
+    # rotations in aziuth do nothing when zenith is zero
+    for az in np.linspace(-1337, 1337, 111):
+        c = {"az": az, "zd": 0, "x": [1, 0, 0], "y": [0, 1, 0], "z": [0, 0, 1]}
+        assert_case(mount="cable_robot_mount", case=c)
+
+    # rotate around x-axis into +y
+    case = {
+        "az": 90,
+        "zd": 45,
+        "x": [1, 0, 0],
+        "y": [0, ISQ, -ISQ],
+        "z": [0, ISQ, ISQ],
+    }
+    assert_case(mount="cable_robot_mount", case=case)
+
+    # rotate around x-axis into -y
+    case = {
+        "az": 90,
+        "zd": -45,
+        "x": [1, 0, 0],
+        "y": [0, ISQ, ISQ],
+        "z": [0, -ISQ, ISQ],
+    }
+    assert_case(mount="cable_robot_mount", case=case)
