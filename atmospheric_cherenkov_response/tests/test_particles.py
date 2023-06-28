829f19d05b595d72ea842db8acb1e393faf429c7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-28 13:41:58 +0200

    test init

---
+++ atmospheric_cherenkov_response/tests/test_particles.py
@@ -0,0 +1,5 @@
+import atmospheric_cherenkov_response as acr
+
+
+def test_all_particles():
+    sites = acr.particles._all()
---
+++ atmospheric_cherenkov_response/tests/test_sites.py
@@ -0,0 +1,5 @@
+import atmospheric_cherenkov_response as acr
+
+
+def test_all_sites():
+    sites = acr.sites._all()
