a0c29569949400ae4f41cac9831df26067a02133
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-12-15 14:21:03 +0100

    allow to compute a single energy bin.

--- atmospheric_cherenkov_response/analysis/__init__.py
+++ atmospheric_cherenkov_response/analysis/__init__.py
@@ -1,16 +1,7 @@
 import numpy as np
 
 
-def divide_silent(numerator, denominator, default):
-    valid = denominator != 0
-    division = np.ones(shape=numerator.shape) * default
-    division[valid] = numerator[valid] / denominator[valid]
-    return division
-
-
 def effective_quantity_for_grid(
-    energy_bin_edges_GeV,
-    energy_GeV,
     mask_detected,
     quantity_scatter,
     num_grid_cells_above_lose_threshold,
@@ -21,29 +12,134 @@ def effective_quantity_for_grid(
 
     Parameters
     ----------
-    energy_bin_edges_GeV            Array of energy-bin-edges in GeV
+    mask_detected : array(num. thrown airshower)
+        A flag/weight for each airshower marking its detection.
+    quantity_scatter : array(num. thrown airshower)
+        The scatter-quantity for each airshower. This is area/m^2 for point
+        like sources, or acceptance/m^2 sr for diffuse sources.
+    num_grid_cells_above_lose_threshold : array(num. thrown airshower)
+        Num. of grid cells passing the lose threshold of the grid for each
+        airshower.
+    total_num_grid_cells : array(num. thrown airshower)
+        The total number of grid-cells thrown for each airshower.
 
-    energy_GeV                      Array(num. thrown airshower)
-                                    The energy of each airshower.
+    Returns
+    -------
+    (effective_quantity, effective_quantity abs. uncertainty) : (float, float)
 
-    mask_detected                   Array(num. thrown airshower)
-                                    A flag/weight for each airshower marking
-                                    its detection.
+    Formula
+    -------
+    Q_effective &=& /frac{ Q_detected }{ C_thrown }
 
-    quantity_scatter                Array(num. thrown airshower)
-                                    The scatter-quantity for each airshower.
-                                    This is area/m^2 for point like sources, or
-                                    acceptance/m^2 sr for diffuse sources.
+    Q_detected &=& /sum_m^M { f_{detected,m} N_{S,m} Q_{scatter,m} }
 
-    num_grid_cells_above_lose_threshold     Array(num. thrown airshower)
-                                            Num. of grid cells passing the lose
-                                            threshold of the grid for each
-                                            airshower.
+    C_thrown &=& /sum_m^M { f_{thrown,m} N_G }
 
-    total_num_grid_cells            Array(num. thrown airshower)
-                                    The total number of grid-cells thrown for
-                                    each airshower.
+    /frac{
+        /Delta_Q_effective
+    }{
+        Q_effective
+    } &=& /frac{
+        /sqrt{ /sum_m^M { f_{detected,m} N_{S,m}^2 } }
+    }{
+        /sum_m^M { f_{detected,m} N_{S,m} }
+    }
 
+    Variables
+    ---------
+    N_G             Num. bins in grid.
+
+    N_{S,m}         Num. bins in grid with cherenkov-photons
+                    above losethreshold for m-th air-shower.
+
+    Q_{scatter,m}   Scatter-quantity of m-th air-shower.
+                    Scatter-area times scatter-solid-angle
+
+    f_{thrown,m}    Flag marking that m-th air-shower is a valid thrown.
+
+    f_{detected,m}  Flag marking that m-th air-shower is a valid detection.
+
+    """
+
+    # sanity checks
+    # -------------
+    assert np.all(mask_detected >= 0)
+    assert np.all(quantity_scatter >= 0.0)
+    assert np.all(num_grid_cells_above_lose_threshold >= 0)
+    assert np.all(total_num_grid_cells > 0)
+
+    _num_events = len(mask_detected)
+    assert _num_events == len(mask_detected)
+    assert _num_events == len(quantity_scatter)
+    assert _num_events == len(num_grid_cells_above_lose_threshold)
+    assert _num_events == len(total_num_grid_cells)
+
+    f_detected = np.asarray(mask_detected, dtype=bool)
+    Q_scatter = np.asarray(quantity_scatter, dtype=float)
+    N_S = np.asarray(num_grid_cells_above_lose_threshold, dtype=np.int64)
+    N_G = np.asarray(total_num_grid_cells, dtype=np.int64)
+
+    # mean
+    # ----
+    Q_detected = np.sum(f_detected * N_S * Q_scatter)
+    C_thrown = np.sum(N_G)
+
+    if C_thrown == 0:
+        Q_effective = 0.0
+    else:
+        Q_effective = Q_detected / C_thrown
+
+    # uncertainty
+    # -----------
+    # according to Werner EffAreaComment.pdf 2020-03-21 17:35
+
+    Sum_f_x_N_S_x_N_S = np.sum(f_detected * N_S**2)
+    assert Sum_f_x_N_S_x_N_S >= 0.0
+
+    Sum_f_x_N_S = np.sum(f_detected * N_S)
+    assert Sum_f_x_N_S >= 0.0
+
+    if Sum_f_x_N_S == 0.0:
+        Q_effective_relunc = 0.0
+    else:
+        Q_effective_relunc = np.sqrt(Sum_f_x_N_S_x_N_S) / Sum_f_x_N_S
+
+    Q_effective_absunc = Q_effective * Q_effective_relunc
+    return Q_effective, Q_effective_absunc
+
+
+def effective_quantity_for_grid_vs_energy(
+    energy_bin_edges_GeV,
+    energy_GeV,
+    mask_detected,
+    quantity_scatter,
+    num_grid_cells_above_lose_threshold,
+    total_num_grid_cells,
+):
+    """
+    Returns the effective quantity and its absolute uncertainty vs. the energy.
+
+    Parameters
+    ----------
+    energy_bin_edges_GeV : array like
+
+    energy_GeV : array like (num. thrown airshower)
+        The energy of each airshower.
+    mask_detected : array(num. thrown airshower)
+        A flag/weight for each airshower marking its detection.
+    quantity_scatter : array(num. thrown airshower)
+        The scatter-quantity for each airshower. This is area/m^2 for point
+        like sources, or acceptance/m^2 sr for diffuse sources.
+    num_grid_cells_above_lose_threshold : array(num. thrown airshower)
+        Num. of grid cells passing the lose threshold of the grid for each
+        airshower.
+    total_num_grid_cells : array(num. thrown airshower)
+        The total number of grid-cells thrown for each airshower.
+
+    Returns
+    -------
+    (effective_quantity, effective_quantity abs. uncertainty) : (array, array)
+        Number of energy bins is len(energy_bin_edges_GeV) - 1.
 
     Formula
     -------
@@ -80,9 +176,35 @@ def effective_quantity_for_grid(
     """
     assert np.all(energy_bin_edges_GeV > 0)
     assert np.all(np.gradient(energy_bin_edges_GeV) > 0)
-
     assert np.all(energy_GeV > 0.0)
 
+    num_energy_bins = len(energy_bin_edges_GeV) - 1
+    assert num_energy_bins >= 1
+
+    Q_effective_vs_energy = np.zeros(num_energy_bins)
+    Q_effective_absunc_vs_energy = np.zeros(num_energy_bins)
+
+    for enbin in range(num_energy_bins):
+        energy_start_GeV = energy_bin_edges_GeV[enbin]
+        energy_stop_GeV = energy_bin_edges_GeV[ebin + 1]
+        energy_mask = np.logical_and(
+            energy_start_GeV <= energy_GeV, energy_stop_GeV > energy_GeV
+        )
+
+        (Q_effective_vs_energy[enbin], Q_effective_absunc_vs_energy[enbin]) = (
+            effective_quantity_for_grid(
+                mask_detected=mask_detected[energy_mask],
+                quantity_scatter=quantity_scatter[energy_mask],
+                num_grid_cells_above_lose_threshold=num_grid_cells_above_lose_threshold[
+                    energy_mask
+                ],
+                total_num_grid_cells=total_num_grid_cells[energy_mask],
+            )
+        )
+
+    return Q_effective_vs_energy, Q_effective_absunc_vs_energy
+
+    """
     assert np.all(mask_detected >= 0)
 
     assert np.all(quantity_scatter >= 0.0)
@@ -144,3 +266,14 @@ def effective_quantity_for_grid(
     )
 
     return effective_quantity, effective_quantity_absolute_uncertainty
+    """
+
+
+"""
+def divide_silent(numerator, denominator, default):
+    valid = denominator != 0
+    division = np.ones(shape=numerator.shape) * default
+    division[valid] = numerator[valid] / denominator[valid]
+    return division
+
+"""
